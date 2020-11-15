a567c36eeb05fada999ab979f38be676a1efc83b
randowlh <1303775690@qq.com>
2020-11-15 19:17:32 +0800

    backup

--- code/tmp/2.cpp
+++ code/tmp/2.cpp
@@ -1,33 +1,41 @@
-#include<bits/stdc++.h>
-#define ull unsigned long long
+#include <bits/stdc++.h>
 using namespace std;
-int _,n,m,len;
-ull ans,a[1000005],b[1000005],w[1000005];
-char s[1000005],str[1000005];
-set<ull> v;
-signed main(){
-    int i,x;
-    w[0]=1;
-    for (i=1;i<=1e6;i++) w[i]=w[i-1]*27;
-    for (scanf("%d",&_);_;_--){
-        scanf("%s",s+1);
-        n=strlen(s+1);
-        a[0]=0;
-        for (i=1;i<=n;i++) a[i]=a[i-1]*27+s[i]-96;
-        for (scanf("%d",&x);x;x--){
-            scanf("%s%d",str+1,&len);
-            m=strlen(str+1);
-            b[0]=0;
-            v.clear();
-            for (i=1;i<=m;i++){
-                b[i]=b[i-1]*27+str[i]-96;
-                if (i>=len) v.insert(b[i]-b[i-len]*w[len]);
-            }
-            ans=0;
-            for (i=len;i<=n;i++)
-            if (v.count(a[i]-a[i-len]*w[len])) ans+=i-len+1;
-            printf("%llu\n",ans);
+void work()
+{
+    int ans=0;
+    for(int i=13930;i<=457439;i++){
+        int t=i;
+        vector<int> v;
+        while(t){
+            v.push_back(t%10);
+            t/=10;
         }
+        int flag=0;
+        for(int j=0;j<v.size()-1;j++){
+            if(abs(v[j]-v[j+1])>7){
+                flag=1;
+                break;
+            }
+        }
+        if(!flag&&abs(v[0]-v[v.size()-1])>2)
+            ans++;
     }
-    return 0;
+    cout<<ans<<endl;
+}
+signed main()
+{
+   #ifndef ONLINE_JUDGE
+   freopen("in.txt","r",stdin);
+//freopen("out.txt","w",stdout);
+#endif
+//std::ios::sync_with_stdio(false);
+//cin.tie(NULL);
+int t = 1;
+//cin>>t;
+while (t--)
+{
+    cout<<180692<<endl;
+//work();
+}
+return 0;
 }
