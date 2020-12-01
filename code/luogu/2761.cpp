6166621bbb16b83a3894fd236488354d0e0fb91b
randowlh <1303775690@qq.com>
2020-12-01 10:08:33 +0800

    back up

--- code/luogu/2761.cpp
+++ code/luogu/2761.cpp
@@ -1,3 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma optimize(2)
+//#pragma GCC optimize("Ofast,no-stack-protector")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+const int inf = 0x7FFFFFFF;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+template<class T>inline void MAX(T &x,T y){if(y>x)x=y;}
+template<class T>inline void MIN(T &x,T y){if(y<x)x=y;}
+template<class T>inline void rd(T &x){
+   x=0;char o,f=1;
+   while(o=getchar(),o<48)if(o==45)f=-f;
+   do x=(x<<3)+(x<<1)+(o^48);
+   while(o=getchar(),o>47);
+   x*=f;
+}
+template<class T>
+void wt(T x){
+   if(x < 0) putchar('-'), x = -x;
+   if(x >= 10) wt(x / 10);
+   putchar('0' + x % 10);
+}
+#define MP make_pair
+#define pb push_back
+#define pt putchar
+#define yx_queue priority_queue
+#define lson(pos) (pos<<1)
+#define rson(pos) (pos<<1|1)
+#define y1 code_by_Rand0w
+#define yn A_muban_for_ACM
+#define j1 it_is just_an_eastegg
+#define lr hope_you_will_be_happy_to_see_this
+// #define int long long
+#define rep(i, a, n) for (register int i = a; i <= n; ++i)
+#define per(i, a, n) for (register int i = n; i >= a; --i)
+const ll llinf = 4223372036854775807;
+const ll mod = (0 ? 1000000007 : 998244353);
+const ll mod2 = 999998639;
+const int m1 = 998244353;
+const int m2 = 1000001011;
+const int pr=233;
+const double eps = 1e-7;
 struct LCF
 {
     struct Edge
@@ -99,3 +143,51 @@ struct LCF
         return flow;
     }
 } tmp;
+string t1,t2;
+int t;
+int n,m;
+void dfs(int step,int fr,int to){
+    if(step==n){
+        tmp.add_edge(fr,to,1,t);
+        return;
+    }
+    int tt=-1;
+    if(t2[step]=='-')
+        tt=0;
+    else if(t2[step]=='+')
+        tt=1;
+    if(t1[step]=='0')
+        for(int i=0;i<=1;i++)
+            dfs(step+1,(fr<<1)+i,(to<<1)+(tt==-1?i:tt));
+    else if(t1[step]=='-'){
+        dfs(step+1,(fr<<1)+0,(to<<1)+(tt==-1?0:tt));
+    }else dfs(step+1,(fr<<1)+1,(to<<1)+(tt==-1?1:tt));
+}
+void work()
+{
+    cin>>n>>m;
+    tmp.init((1<<n)+4,(1<<n)-1,1<<n);
+    tmp.add_edge(0,(1<<n),1,0);
+    for(int i=1;i<=m;i++){
+        cin>>t>>t1>>t2;
+        dfs(0,0,0);
+    }
+    tmp.cost_flow();
+    cout<<tmp.ans<<endl;
+}
+signed main()
+{
+   #ifndef ONLINE_JUDGE
+   freopen("in.txt","r",stdin);
+//freopen("out.txt","w",stdout);
+#endif
+std::ios::sync_with_stdio(false);
+cin.tie(NULL);
+int t = 1;
+//cin>>t;
+while (t--)
+{
+work();
+}
+return 0;
+}
