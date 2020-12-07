1445084c5a043431c38900f5b2fec00687bfb7c8
Rand0w <rand0w@Rand0wdeMacBook.local>
2020-12-07 12:41:13 +0800

    back up

--- code/tmp/2.cpp
+++ code/tmp/2.cpp
@@ -1,20 +1,9 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
 int main(){
-    int t;
-    scanf("%d",&t);
-    for(int q=0;q<t;q++){
-        int n,k;
-        scanf("%d%d",&n,&k);
-        int cnt=0;
-        for(int i=1;i<=n;i++){
-            int a,b;
-            scanf("%d%d",&a,&b);
-            if(b<k)
-                continue;
-            if((double)a/(double)b<0.5)
-                continue;
-            cnt++;
-        }
-        printf("%d\n",cnt);
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    for(int i=1;i<n;i++){
     }
 }
