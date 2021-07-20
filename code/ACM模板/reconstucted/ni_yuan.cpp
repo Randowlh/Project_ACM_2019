ca7bbb4e02a80d372ad17a04be148d87e0b3090e
randowlh <1303775690@qq.com>
2021-07-20 17:38:26 +0800

    back up

--- code/ACM模板/reconstucted/ni_yuan.cpp
+++ code/ACM模板/reconstucted/ni_yuan.cpp
@@ -1,16 +1,16 @@
-inline int pow(int x, int a, int mod)
-{
-    int ans = 0;
-    int now = x;
-    while (a != 0)
-    {
-        if (a & 1)
-        {
-            ans = ans * now % mod;
-        }
-        now = now * now % mod;
-        a >>= 1;
+int exgcd(int a, int b, int& x, int& y) {
+    x = 1, y = 0;
+    int x1 = 0, y1 = 1, a1 = a, b1 = b;
+    while (b1) {
+      int q = a1 / b1;
+      tie(x, x1) = make_tuple(x1, x - q * x1);
+      tie(y, y1) = make_tuple(y1, y - q * y1);
+      tie(a1, b1) = make_tuple(b1, a1 - q * b1);
     }
+    return a1;
+}
+inline int niyuan(int x, int mod) { 
+    int ans,tmp;
+    exgcd(x,mod,ans,tmp);
     return ans;
 }
-inline int niyuan(int x, int mod) { return pow(x, mod - 2, mod); }
