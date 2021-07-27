13eb24dc0cd1497666c2d6cc720e385de0a2f910
randowlh <1303775690@qq.com>
2021-07-27 20:49:04 +0800

    back up

--- code/competition/duoxiao3/1.cpp
+++ code/competition/duoxiao3/1.cpp
@@ -13,7 +13,6 @@ typedef double db;
 typedef long double ld;
 template<class T>inline void MAX(T &x,T y){if(y>x)x=y;}
 template<class T>inline void MIN(T &x,T y){if(y<x)x=y;}
-const ll mod = (0 ? 1000000007 : 998244353);
 namespace FastIO
 {
 char buf[1 << 21], buf2[1 << 21], a[20], *p1 = buf, *p2 = buf, hh = '\n';
@@ -47,56 +46,6 @@ buf2[++p3] = hh;
 print(oth...);
 }
 } // namespace FastIO
-struct mint
-{
-    ll n;
-    mint() : n(0) { ; }
-    mint(ll m) : n(m)
-    {
-        if (n >= mod)
-            n %= mod;
-        else if (n < 0)
-            n = (n % mod + mod) % mod;
-    }
-    inline operator int() { return n; }
-    inline bool operator==(mint b) { return n == b.n; }
-    inline mint operator+=(mint b)
-    {
-        n += b.n;
-        if (n >= mod)
-            n -= mod;
-        return *this;
-    }
-    mint operator-=(mint b)
-    {
-        n -= b.n;
-        if (n < 0)
-            n += mod;
-        return *this;
-    }
-    mint operator*=( mint b)
-    {
-        n = ((ll)n * n) % mod;
-        return *this;
-    }
-};
-mint operator+(mint a, mint b) { return a += b; }
-mint operator-(mint a, mint b) { return a -= b; }
-mint operator*(mint a, mint b) { return a *= b; }
-mint operator^(mint a, int n)
-{
-    if (n == 0)
-        return mint(1);
-    mint res = (a * a) ^ (n / 2);
-    if (n % 2)
-        res = res * a;
-    return res;
-}
-ll inv(ll a, ll p)
-{
-    return (a == 1 ? 1 : (1 - p * inv(p % a, a)) / a + p);
-}
-mint operator/(mint a, mint b) { return a * mint(inv(b, mod)); }
 #define read FastIO::read
 #define print FastIO::print
 #define flush FastIO::flush
@@ -120,7 +69,7 @@ mint operator/(mint a, mint b) { return a * mint(inv(b, mod)); }
 #define rep(i, a, n) for (register int i = a; i <= n; ++i)
 #define per(i, a, n) for (register int i = n; i >= a; --i)
 const ll llinf = 4223372036854775807;
-
+const ll mod = (0 ? 1000000007 : 998244353);
 ll pow(ll a,ll b,ll md=mod) {ll res=1;a%=md; assert(b>=0); for(;b;b>>=1){if(b&1)res=mul(res,a,md);a=mul(a,a,md);}return res;}
 const ll mod2 = 999998639;
 const int m1 = 998244353;
@@ -129,38 +78,43 @@ const int pr=233;
 const double eps = 1e-7;
 const int maxm= 1;
 const int maxn = 510000;
-mint f[110],f2[110];
-mint w[110];
+map<int,int> M;
+inline double run_time(){
+    return 1.0*clock()/CLOCKS_PER_SEC;
+}
 void work()
 {
-    int tmp;
-    int n;
-    cin>>n;
-    mint tot=0;
-    for(int i=1;i<=n;i++)
-        cin>>tmp,tot+=tmp,w[i]=tmp;
-    mint q=w[1]/tot;
-    f[1]=q/((q-(mint)1)*(q-(mint)1));
-    f2[1]=q*(q+(mint)1)/(((mint)1-q)*((mint)1-q)*((mint)1-q));
-    for(int i=2;i<=n;i++){
-        mint q=w[i]/tot;
-        f[i]=f[i-1]+(q)/(((mint)1-q)*((mint)1-q));
-        f2[i]=f2[i-1];
-        f2[i]+=f[i-1]*(mint)2*(q)/(((mint)1-q)*((mint)1-q));
-        f2[i]+=q*(q+(mint)1)/(((mint)1-q)*((mint)1-q)*((mint)1-q));
+    M.clear();
+    int n,k;
+    cin>>n>>k;
+    M[n]=1;
+    int ans=0;
+    for(auto i=M.find(n);;i--){
+        ans+=i->second;
+        if(i->first<=k){
+            if(i==M.begin())
+                break;
+            continue;
+        }
+        M[i->first/2]+=i->second;
+        M[(i->first+1)/2]+=i->second;
+        // i=M.find(i->first);
+        if(i==M.begin())
+                break;
     }
-    cout<<f2[n]+f[n]*(mint)2+mint(1)<<endl;
+    cout<<ans<<endl;
+    // cout<<run_time()<<endl;
 }
 signed main()
 {
    #ifndef ONLINE_JUDGE
-   freopen("in.txt","r",stdin);
-    //freopen("out.txt","w",stdout);
+//    freopen("in.txt","r",stdin);
+    // freopen("out.txt","w",stdout);
 #endif
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
     int t = 1;
-    // cin>>t;
+    cin>>t;
     while (t--)
     {
         work();
--- code/competition/duoxiao3/3.cpp
+++ code/competition/duoxiao3/3.cpp
@@ -4,8 +4,8 @@
 //using namespace __gnu_cxx;
 using namespace std;
 #pragma optimize(2)
-//#pragma GCC optimize("Ofast,no-stack-protector")
-//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
 #define rbset(T) tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>
 const int inf = 0x7FFFFFFF;
 typedef long long ll;
@@ -13,7 +13,6 @@ typedef double db;
 typedef long double ld;
 template<class T>inline void MAX(T &x,T y){if(y>x)x=y;}
 template<class T>inline void MIN(T &x,T y){if(y<x)x=y;}
-const ll mod = (0 ? 1000000007 : 998244353);
 namespace FastIO
 {
 char buf[1 << 21], buf2[1 << 21], a[20], *p1 = buf, *p2 = buf, hh = '\n';
@@ -47,56 +46,6 @@ buf2[++p3] = hh;
 print(oth...);
 }
 } // namespace FastIO
-struct mint
-{
-    ll n;
-    mint() : n(0) { ; }
-    mint(ll m) : n(m)
-    {
-        if (n >= mod)
-            n %= mod;
-        else if (n < 0)
-            n = (n % mod + mod) % mod;
-    }
-    inline operator int() { return n; }
-    inline bool operator==(mint b) { return n == b.n; }
-    inline mint operator+=(mint b)
-    {
-        n += b.n;
-        if (n >= mod)
-            n -= mod;
-        return *this;
-    }
-    mint operator-=(mint b)
-    {
-        n -= b.n;
-        if (n < 0)
-            n += mod;
-        return *this;
-    }
-    mint operator*=( mint b)
-    {
-        n = ((ll)n * n) % mod;
-        return *this;
-    }
-};
-mint operator+(mint a, mint b) { return a += b; }
-mint operator-(mint a, mint b) { return a -= b; }
-mint operator*(mint a, mint b) { return a *= b; }
-mint operator^(mint a, int n)
-{
-    if (n == 0)
-        return mint(1);
-    mint res = (a * a) ^ (n / 2);
-    if (n % 2)
-        res = res * a;
-    return res;
-}
-ll inv(ll a, ll p)
-{
-    return (a == 1 ? 1 : (1 - p * inv(p % a, a)) / a + p);
-}
-mint operator/(mint a, mint b) { return a * mint(inv(b, mod)); }
 #define read FastIO::read
 #define print FastIO::print
 #define flush FastIO::flush
@@ -116,11 +65,11 @@ mint operator/(mint a, mint b) { return a * mint(inv(b, mod)); }
 #define yn A_muban_for_ACM
 #define j1 it_is just_an_eastegg
 #define lr hope_you_will_be_happy_to_see_this
-#define int long long
+// #define int long long
 #define rep(i, a, n) for (register int i = a; i <= n; ++i)
 #define per(i, a, n) for (register int i = n; i >= a; --i)
 const ll llinf = 4223372036854775807;
-
+const ll mod = (0 ? 1000000007 : 998244353);
 ll pow(ll a,ll b,ll md=mod) {ll res=1;a%=md; assert(b>=0); for(;b;b>>=1){if(b&1)res=mul(res,a,md);a=mul(a,a,md);}return res;}
 const ll mod2 = 999998639;
 const int m1 = 998244353;
@@ -128,28 +77,107 @@ const int m2 = 1000001011;
 const int pr=233;
 const double eps = 1e-7;
 const int maxm= 1;
-const int maxn = 510000;
-mint f[110],f2[110];
-mint w[110];
+const int maxn = 110000;
+string cst="0123456789ABCDEF";
+int seg[maxn<<2][3];
+int c[maxn][3];
+int dp[maxn];
+string biao[300];
+struct node{
+    int r,g,b;
+    node(int a,int b,int c):r(a),g(b),b(c) {}
+    node(){}
+};
+inline void pushup(int pos){
+    // for(int i=0;i<3;i++)
+    seg[pos][0]=seg[lson(pos)][0]+seg[rson(pos)][0];
+    seg[pos][1]=seg[lson(pos)][1]+seg[rson(pos)][1];
+    seg[pos][2]=seg[lson(pos)][2]+seg[rson(pos)][2];
+}
+void build(int pos,int l,int r){
+    if(l==r){
+        seg[pos][0]=c[l][0]; 
+        seg[pos][1]=c[l][1];     
+        seg[pos][2]=c[l][2];  
+        return;
+    }
+    int mid=(l+r)>>1;
+    build(lson(pos),l,mid);
+    build(rson(pos),mid+1,r);
+    pushup(pos);
+}
+node query(int pos,int l,int r,int ql,int qr){
+    if(l>=ql&&r<=qr){
+        return node(seg[pos][0],seg[pos][1],seg[pos][2]);
+    }
+    int mid=(l+r)>>1;
+    node ans(0,0,0);
+    if(ql<=mid){
+        node tt=query(lson(pos),l,mid,ql,qr);
+        ans.b+=tt.b;
+        ans.g+=tt.g;
+        ans.r+=tt.r;
+    }
+    if(qr>=mid+1){
+        node tt=query(rson(pos),mid+1,r,ql,qr);
+        ans.b+=tt.b;
+        ans.g+=tt.g;
+        ans.r+=tt.r;
+    }
+    return ans;
+}
+inline int trans(string er){
+    int ans=0;
+    if(!isdigit(er[0])){
+        ans+=160;
+        ans+=16*(er[0]-'A');
+    }else{
+        ans+=16*(er[0]-'0');
+    }
+    if(!isdigit(er[1])){
+        ans+=10;
+        ans+=(er[1]-'A');
+    }else{
+        ans+=(er[1]-'0');
+    }
+    return ans;
+}
+inline string rtans(int t){
+    string ans;
+    ans.push_back(cst[t/16]);
+    ans.push_back(cst[t%16]);
+    return ans;
+}
 void work()
 {
-    int tmp;
-    int n;
-    cin>>n;
-    mint tot=0;
-    for(int i=1;i<=n;i++)
-        cin>>tmp,tot+=tmp,w[i]=tmp;
-    mint q=w[1]/tot;
-    f[1]=q/((q-(mint)1)*(q-(mint)1));
-    f2[1]=q*(q+(mint)1)/(((mint)1-q)*((mint)1-q)*((mint)1-q));
-    for(int i=2;i<=n;i++){
-        mint q=w[i]/tot;
-        f[i]=f[i-1]+(q)/(((mint)1-q)*((mint)1-q));
-        f2[i]=f2[i-1];
-        f2[i]+=f[i-1]*(mint)2*(q)/(((mint)1-q)*((mint)1-q));
-        f2[i]+=q*(q+(mint)1)/(((mint)1-q)*((mint)1-q)*((mint)1-q));
+    int n,q;
+    cin>>n>>q;
+    string tmp;
+    for(int i=1;i<=n;i++){
+        cin>>dp[i]>>tmp;
+        if(dp[i]!=1)
+            dp[i]=0;
+        c[i][0]=trans(tmp.substr(0,2));
+        c[i][1]=trans(tmp.substr(2,2));
+        c[i][2]=trans(tmp.substr(4,2));
     }
-    cout<<f2[n]+f[n]*(mint)2+mint(1)<<endl;
+    build(1,1,n);
+    int now=0;
+    for(int i=1;i<=n;i++){
+        if(dp[i])
+            now=i;
+        dp[i]=now;
+    }
+    int l,r;
+    for(int i=1;i<=q;i++){
+        cin>>l>>r;
+        MAX(l,dp[r]);
+        node tt=query(1,1,n,l,r);
+        MIN(tt.r,255);
+        MIN(tt.g,255);
+        MIN(tt.b,255);
+        cout<<biao[tt.r]<<biao[tt.g]<<biao[tt.b]<<'\n';
+    }
 }
 signed main()
 {
@@ -160,7 +188,9 @@ signed main()
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
     int t = 1;
-    // cin>>t;
+    for(int i=0;i<=255;i++)
+        biao[i]=rtans(i);
+    cin>>t;
     while (t--)
     {
         work();
