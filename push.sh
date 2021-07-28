1b1f5c13f454e697ab0ceb0cbb4be31c5e880233
randowlh <1303775690@qq.com>
2021-07-28 20:12:20 +0800

    back up

---
+++ push.sh
@@ -0,0 +1,6 @@
+cd Desktop/Project_ACM_renewed
+find ./ -name '*.exe' -type f -print -exec rm -rf {} \;
+find ./ -name '*.bin' -type f -print -exec rm -rf {} \;
+git add -A
+git commit -a -m 'back up'
+git push origin main 
